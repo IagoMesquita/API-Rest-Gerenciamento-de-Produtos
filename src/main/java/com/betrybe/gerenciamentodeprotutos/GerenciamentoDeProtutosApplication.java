abb8b8870a4d9178bb4d8434efbdee0993dabd2e
Iago Mesquita <iagomesquitaalves@gmail.com>
2024-05-01 08:50:57 -0300

    Inital commit

---
+++ src/main/java/com/betrybe/gerenciamentodeprotutos/GerenciamentoDeProtutosApplication.java
@@ -0,0 +1,13 @@
+package com.betrybe.gerenciamentodeprotutos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GerenciamentoDeProtutosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GerenciamentoDeProtutosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/betrybe/gerenciamentodeprotutos/GerenciamentoDeProtutosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.betrybe.gerenciamentodeprotutos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GerenciamentoDeProtutosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
