532ab0b109ec9dfa723d4867f31ce1f756099c71
Iago Mesquita <iagomesquitaalves@gmail.com>
2024-05-02 19:43:54 -0300

    feat:add methods convert

--- src/main/java/com/betrybe/gerenciamentodeprotutos/dto/ProductCreationDto.java
+++ src/main/java/com/betrybe/gerenciamentodeprotutos/dto/ProductCreationDto.java
@@ -1,5 +1,7 @@
 package com.betrybe.gerenciamentodeprotutos.dto;
 
 public record ProductCreationDto(String name, Double price) {
-
+  public ProductCreationDto toEntity() {
+    return new ProductCreationDto(name, price);
+  }
 }
--- src/main/java/com/betrybe/gerenciamentodeprotutos/dto/ProtuctDto.java
+++ src/main/java/com/betrybe/gerenciamentodeprotutos/dto/ProtuctDto.java
@@ -1,5 +1,13 @@
 package com.betrybe.gerenciamentodeprotutos.dto;
 
-public record ProtuctDto(Long id, String name, Double price) {
+import com.betrybe.gerenciamentodeprotutos.entity.Product;
 
+public record ProtuctDto(Long id, String name, Double price) {
+  public static ProtuctDto fromEntity(Product product) {
+    return new ProtuctDto(
+        product.getId(),
+        product.getName(),
+        product.getPrice()
+    );
+  }
 }
