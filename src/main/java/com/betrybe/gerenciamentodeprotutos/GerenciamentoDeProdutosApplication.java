579c99254a8e2c1c56d6c3cf4aba62ad89da02dc
Iago Mesquita <iagomesquitaalves@gmail.com>
2024-05-02 08:14:16 -0300

    feat:lint main programmer

--- src/main/java/com/betrybe/gerenciamentodeprotutos/GerenciamentoDeProdutosApplication.java
+++ src/main/java/com/betrybe/gerenciamentodeprotutos/GerenciamentoDeProdutosApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class GerenciamentoDeProdutosApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(GerenciamentoDeProdutosApplication.class, args);
-	}
+  public static void main(String[] args) {
+    SpringApplication.run(GerenciamentoDeProdutosApplication.class, args);
+  }
 
 }
