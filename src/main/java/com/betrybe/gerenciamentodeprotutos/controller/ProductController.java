0bac2844355fc84f51d47f2e4e518a79266728f6
Iago Mesquita <iagomesquitaalves@gmail.com>
2024-05-02 19:43:23 -0300

    refactor:comment ExceptionHandler

--- src/main/java/com/betrybe/gerenciamentodeprotutos/controller/ProductController.java
+++ src/main/java/com/betrybe/gerenciamentodeprotutos/controller/ProductController.java
@@ -9,7 +9,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -53,12 +52,12 @@ public class ProductController {
 
   }
 
-  @ExceptionHandler
-  public ResponseEntity<String> handleProductNotFoundException(ProductNotFoundException exception) {
-    return ResponseEntity
-        .status(HttpStatus.NOT_FOUND)
-        .body(exception.getMessage());
-  }
+//  @ExceptionHandler
+//  public ResponseEntity<String> handleProductNotFoundException(ProductNotFoundException exception) {
+//    return ResponseEntity
+//        .status(HttpStatus.NOT_FOUND)
+//        .body(exception.getMessage());
+//  }
 
   @PutMapping("/{productId}")
   public ResponseEntity<Product> updateProduct(
