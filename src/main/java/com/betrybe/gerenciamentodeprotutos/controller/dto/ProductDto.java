cc565bb5544929b0987e36e5f1b19eba4a58e1a9
Iago Mesquita <iagomesquitaalves@gmail.com>
2024-05-08 07:46:04 -0300

    refactor:add ProductDetailsDto and BrandDto in ProductDto

--- src/main/java/com/betrybe/gerenciamentodeprotutos/controller/dto/ProductDto.java
+++ src/main/java/com/betrybe/gerenciamentodeprotutos/controller/dto/ProductDto.java
@@ -2,12 +2,21 @@ package com.betrybe.gerenciamentodeprotutos.controller.dto;
 
 import com.betrybe.gerenciamentodeprotutos.model.entity.Product;
 
-public record ProductDto(Long id, String name, Double price) {
+public record ProductDto(Long id, String name, Double price, ProductDetailsDto details, BrandDto brand) {
+
   public static ProductDto fromEntity(Product product) {
+    BrandDto brandDto = product.getBrand() != null ?
+        BrandDto.fromEntity(product.getBrand()) : null;
+
+    ProductDetailsDto productDetailsDto = product.getProductDetail() != null ?
+    ProductDetailsDto.fromEntity(product.getProductDetail()) : null;
+
     return new ProductDto(
         product.getId(),
         product.getName(),
-        product.getPrice()
+        product.getPrice(),
+        productDetailsDto,
+        brandDto
     );
   }
 }
