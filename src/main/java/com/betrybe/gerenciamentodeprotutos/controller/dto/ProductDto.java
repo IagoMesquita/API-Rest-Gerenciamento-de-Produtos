b6c7c51b2f36c2aee7c708d846543151755d5672
Iago Mesquita <iagomesquitaalves@gmail.com>
2024-05-08 21:34:17 -0300

    refactor:add CategoryDto in ProductDto

--- src/main/java/com/betrybe/gerenciamentodeprotutos/controller/dto/ProductDto.java
+++ src/main/java/com/betrybe/gerenciamentodeprotutos/controller/dto/ProductDto.java
@@ -1,22 +1,33 @@
 package com.betrybe.gerenciamentodeprotutos.controller.dto;
 
 import com.betrybe.gerenciamentodeprotutos.model.entity.Product;
+import java.util.List;
 
-public record ProductDto(Long id, String name, Double price, ProductDetailsDto details, BrandDto brand) {
+public record ProductDto(
+    Long id,
+    String name,
+    Double price,
+    ProductDetailsDto details,
+    BrandDto brand,
+    List<CategoryDto> categories) {
 
   public static ProductDto fromEntity(Product product) {
     BrandDto brandDto = product.getBrand() != null ?
         BrandDto.fromEntity(product.getBrand()) : null;
 
     ProductDetailsDto productDetailsDto = product.getProductDetail() != null ?
-    ProductDetailsDto.fromEntity(product.getProductDetail()) : null;
+        ProductDetailsDto.fromEntity(product.getProductDetail()) : null;
+
+    List<CategoryDto> categoryDto = product.getCategories().stream().map(CategoryDto::fromEntity)
+        .toList();
 
     return new ProductDto(
         product.getId(),
         product.getName(),
         product.getPrice(),
         productDetailsDto,
-        brandDto
-    );
+        brandDto,
+        categoryDto
+        );
   }
 }
